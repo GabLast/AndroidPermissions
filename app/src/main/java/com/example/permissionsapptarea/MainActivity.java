f5000baf7db90dc085730e8cd668851cdece2433
Gabriel <20170388@ce.pucmm.edu.do>
2021-09-28 20:29:37 -0400

    done

--- app/src/main/java/com/example/permissionsapptarea/MainActivity.java
+++ app/src/main/java/com/example/permissionsapptarea/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.permissionsapptarea;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private Switch camera;
     private Switch phone;
     private Switch contacts;
+    private List<String> reqPerms = new ArrayList<>();
+    private final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +41,132 @@ public class MainActivity extends AppCompatActivity {
         camera = findViewById(R.id.cameraSwitch);
         phone = findViewById(R.id.phoneSwitch);
         contacts = findViewById(R.id.contactSwitch);
+        verifyPermissions();
+
+//        continueView.setOnClickListener(view -> {
+//            onContinue();
+//        });
+    }
+
+    public void killApp(View view) {
+
+        if(view.getId() == cancelView.getId())
+        {
+            finishAndRemoveTask();
+        }
+    }
+
+    public void onContinue(View view) {
+
+        if(view.getId() == continueView.getId())
+        {
+            if (storage.isChecked() &&
+                    ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                reqPerms.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+            if (location.isChecked() &&
+                    ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                reqPerms.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (camera.isChecked() &&
+                    ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                reqPerms.add(Manifest.permission.CAMERA);
+            }
+            if (phone.isChecked() &&
+                    ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED) {
+                reqPerms.add(Manifest.permission.CALL_PHONE);
+            }
+            if (contacts.isChecked() &&
+                    ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_DENIED) {
+                reqPerms.add(Manifest.permission.READ_CONTACTS);
+            }
+
+            if(!reqPerms.isEmpty())
+            {
+                ActivityCompat.requestPermissions(this, reqPerms.toArray(new String[reqPerms.size()]), PERMISSION_REQUEST_CODE);
+            }else {
+                Intent intent = new Intent(getApplicationContext(), PermissionsActivity.class);
+                startActivity(intent);
+
+            }
+        }
 
     }
 
-    public void killApp()
-    {
-        this.finish();
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Intent intent = new Intent(getApplicationContext(), PermissionsActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this, "All permissions were denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
+    public void verifyPermissions(){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+           storage.setChecked(true);
 
+        }
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            location.setChecked(true);
+
+        }
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            camera.setChecked(true);
+
+        }
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+           phone.setChecked(true);
+
+        }
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            contacts.setChecked(true);
+
+        }
+    }
+
+    public void permissionGranted(View view){
+
+        if(view.getId() == R.id.storageSwitch &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+
+            storage.setChecked(true);
+            Snackbar.make(view,"Storage permissions can't be revoked from here", Snackbar.LENGTH_LONG).show();
+
+        }else if(view.getId() == R.id.locationSwitch &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+            location.setChecked(true);
+            Snackbar.make(view,"Location permissions can't be revoked from here", Snackbar.LENGTH_LONG).show();
+
+        }else if(view.getId() == R.id.cameraSwitch &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+
+            camera.setChecked(true);
+            Snackbar.make(view,"Camera permissions can't be revoked from here", Snackbar.LENGTH_LONG).show();
+
+        }else if(view.getId() == R.id.phoneSwitch &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+
+            phone.setChecked(true);
+            Snackbar.make(view,"Phone calls permissions can't be revoked from here", Snackbar.LENGTH_LONG).show();
+
+        }else if(view.getId() == R.id.contactSwitch &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+
+            contacts.setChecked(true);
+            Snackbar.make(view,"Contacts access permissions can't be revoked from here", Snackbar.LENGTH_LONG).show();
+        }
+
+    }
 }
--- app/src/main/java/com/example/permissionsapptarea/PermissionsActivity.java
+++ app/src/main/java/com/example/permissionsapptarea/PermissionsActivity.java
@@ -1,26 +1,146 @@
 package com.example.permissionsapptarea;
 
-import android.widget.TextView;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+import com.google.android.material.snackbar.Snackbar;
 
 public class PermissionsActivity extends AppCompatActivity {
 
-    private TextView storageTextView;
-    private TextView locationTextView;
-    private TextView cameraTextView;
-    private TextView phoneTextView;
-    private TextView contactsTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
 
-        storageTextView = findViewById(R.id.storageTxtView);
-        locationTextView = findViewById(R.id.locationTxtView);
-        cameraTextView = findViewById(R.id.cameraTxtView);
-        phoneTextView = findViewById(R.id.phoneTxtView);
-        contactsTextView = findViewById(R.id.contactTxtView);
     }
+
+
+    public void openStorage(View view){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+//            https://stackoverflow.com/questions/58550549/how-to-use-intent-action-open-document-in-android-pie
+            Snackbar snack = Snackbar.make(view,"Press OPEN to proceed", Snackbar.LENGTH_LONG);
+            snack.setAction("OPEN", new ReadStorage());
+            snack.show();
+        }else {
+            Snackbar.make(view,"No storage permissions have been granted", Snackbar.LENGTH_LONG).show();
+        }
+
+    }
+
+    public class ReadStorage implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("application/*");
+            startActivity(intent);
+        }
+    }
+
+    public void openLocation(View view){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//           https://developers.google.com/maps/documentation/urls/android-intents
+            Snackbar snack = Snackbar.make(view,"Press OPEN to proceed", Snackbar.LENGTH_LONG);
+            snack.setAction("OPEN", new OpenMap());
+            snack.show();
+        }else {
+            Snackbar.make(view,"No location permissions have been granted", Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    public class OpenMap implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            intent.setPackage("com.google.android.apps.maps");
+            startActivity(intent);
+        }
+    }
+
+
+    public void openCamera(View view){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+            Snackbar snack = Snackbar.make(view,"Press OPEN to proceed", Snackbar.LENGTH_LONG);
+            snack.setAction("OPEN", new openCam());
+            snack.show();
+        }else {
+            Snackbar.make(view,"No camera permissions have been granted", Snackbar.LENGTH_LONG).show();
+        }
+
+    }
+
+    public class openCam implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(intent);
+        }
+    }
+
+    public void openPhone(View view){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+//          https://stackoverflow.com/questions/4275678/how-to-make-a-phone-call-using-intent-in-android
+            Snackbar snack = Snackbar.make(view,"Press OPEN to proceed", Snackbar.LENGTH_LONG);
+            snack.setAction("OPEN", new MakeACall());
+            snack.show();
+        }else {
+            Snackbar.make(view,"No phone calls permissions have been granted", Snackbar.LENGTH_LONG).show();
+        }
+
+    }
+
+    public class MakeACall implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+
+            String uri = "tel:" + "123-456-7890";
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse(uri));
+            startActivity(intent);
+        }
+    }
+
+    public void openContacts(View view){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+//            https://stackoverflow.com/questions/9955783/how-do-i-open-contacts-when-i-click-a-button-defined-in-main-xml
+            Snackbar snack = Snackbar.make(view,"Press OPEN to proceed", Snackbar.LENGTH_LONG);
+            snack.setAction("OPEN", new OpenContacts());
+            snack.show();
+        }else {
+            Snackbar.make(view,"No contacts permissions have been granted", Snackbar.LENGTH_LONG).show();
+        }
+
+    }
+
+    public class OpenContacts implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent(Intent.ACTION_PICK,  ContactsContract.Contacts.CONTENT_URI);
+            startActivity(intent);
+        }
+    }
+
 }
